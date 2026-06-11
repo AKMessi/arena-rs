522e0aab7fea926c4947ddf8e36c31d6d5e6ea1f
AKMessi <kakadaaryan10@gmail.com>
2026-06-11 09:58:25 +0530

    feat: implement m3 projectile mechanics using single system param

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,4 @@
-use bevy::{math::VectorSpace, prelude::*, render::render_resource::{CachedPipelineState::Ok, Pipeline}};
+use bevy::prelude::*;
 
 fn main() {
     App::new()
@@ -11,6 +11,15 @@ fn main() {
         }))
         .insert_resource(ClearColor(Color::BLACK))
         .add_systems(Startup, setup_system)
+        .add_systems(
+            Update,
+            (
+                player_input_system,
+                movement_system,
+                bullet_spawner_system,
+                bullet_movement_system,
+            ),
+        )
         .run();
 }
 
@@ -19,6 +28,9 @@ struct Player;
 
 #[derive(Component)]
 struct Velocity(Vec2);
+
+#[derive(Component)]
+struct Bullet;
 
 fn setup_system(mut commands: Commands) {
     commands.spawn(Camera2d);
@@ -37,23 +49,54 @@ fn setup_system(mut commands: Commands) {
 
 fn player_input_system(
     keyboard_input: Res<ButtonInput<KeyCode>>,
-    mut query: Query<&mut Velocity, With<Player>>,
+    mut player_velocity: Single<&mut Velocity, With<Player>>,
 ) {
-    if let Ok(mut Velocity) = query.get_single_mut() {
         let mut direction = Vec2::ZERO;
-
-        if keyboard_input.pressed(KeyCode::KeyW) { direction.y += 1.0; }
-        if keyboard_input.pressed(KeyCode::KeyS) { direction.y -= 1.0; }
-        if keyboard_input.pressed(KeyCode::KeyA) { direction.x -= 1.0; }
-        if keyboard_input.pressed(KeyCode::KeyD) { direction.x += 1.0; }
-
-        let speed = 400.0;
-        velocity.0 = direction.normalize_or_zero() * speed;
+    
+        if keyboard_input.pressed(KeyCode::KeyW) {
+                direction.y += 1.0;
+        }
+        if keyboard_input.pressed(KeyCode::KeyS) {
+                direction.y -= 1.0;
+        }
+        if keyboard_input.pressed(KeyCode::KeyA) {
+                direction.x -= 1.0;
+        }
+        if keyboard_input.pressed(KeyCode::KeyD) {
+                direction.x += 1.0;
     }
+
+    let speed = 400.0;
+    player_velocity.0 = direction.normalize_or_zero() * speed;
 }
 
 fn movement_system(time: Res<Time>, mut query: Query<(&mut Transform, &Velocity)>) {
     for (mut transform, velocity) in &mut query {
         transform.translation += velocity.0.extend(0.0) * time.delta_secs();
+    }
+}
+
+fn bullet_spawner_system(
+    mut commands: Commands,
+    keyboard_input: Res<ButtonInput<KeyCode>>,
+    player_transform: Single<&Transform, With<Player>>,
+) {
+    if keyboard_input.just_pressed(KeyCode::Space) {
+        commands.spawn((
+            Sprite {
+                custom_size: Some(Vec2::new(8.0, 16.0)),
+                color: Color::srgb(1.0, 1.0, 0.0),
+                ..default()
+                },
+            Transform::from_translation(player_transform.translation),
+            Bullet,
+            ));
+    }
+}
+
+fn bullet_movement_system(time: Res<Time>, mut query: Query<&mut Transform, With<Bullet>>) {
+    let bullet_speed = 700.0;
+    for mut transform in &mut query {
+        transform.translation.y += bullet_speed * time.delta_secs();
     }
 }
